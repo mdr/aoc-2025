a12800957f1109866fd8de508a2bb474fa7ee487
Matt Russell <MattRussellUK@gmail.com>
2025-12-08 07:58:50 +0000

    day 8 part 1

---
+++ src/main/scala/aoc2025/day08/Day08.scala
@@ -0,0 +1,76 @@
+package aoc2025.day08
+
+import aoc2025.utils.readInput
+
+case class Point3D(x: Double, y: Double, z: Double):
+  def distanceTo(other: Point3D): Double =
+    math.sqrt(
+      math.pow(x - other.x, 2) +
+        math.pow(y - other.y, 2) +
+        math.pow(z - other.z, 2)
+    )
+
+object Point3D:
+  given Ordering[Point3D] = Ordering.by(p => (p.x, p.y, p.z))
+
+  private def parse(s: String): Point3D =
+    val Seq(x, y, z) = s.split(",").map(_.trim.toDouble).toSeq
+    Point3D(x, y, z)
+
+  def parsePoints(input: String): Seq[Point3D] =
+    input.linesIterator.map(Point3D.parse).toSeq
+
+  def findClosestPairs(points: Set[Point3D]): Seq[(Point3D, Point3D)] =
+    points.subsets(2).map { pair =>
+      val Seq(p1, p2) = pair.toSeq.sorted
+      (p1, p2)
+    }.toSeq.sortBy { case (p1, p2) => p1.distanceTo(p2) }
+
+case class Circuit(boxes: Set[Point3D]):
+  def contains(box: Point3D): Boolean = boxes.contains(box)
+
+  def merge(other: Circuit): Circuit =
+    Circuit(this.boxes ++ other.boxes)
+
+case class Playground(allBoxes: Set[Point3D], circuits: Set[Circuit]):
+  def score: Long = circuits.toSeq.map(_.boxes.size.toLong).sorted.takeRight(3).product
+
+  private def getCircuitContaining(box: Point3D): Circuit =
+    circuits.find(_.contains(box)) getOrElse
+      (throw new IllegalArgumentException(s"Box $box not found in any circuit"))
+
+  private def connectBoxes(box1: Point3D, box2: Point3D): Playground =
+    val circuit1 = getCircuitContaining(box1)
+    val circuit2 = getCircuitContaining(box2)
+    val newCircuit = circuit1.merge(circuit2)
+    val newCircuits = circuits - circuit1 - circuit2 + newCircuit
+    this.copy(circuits = newCircuits)
+
+  def connect(numberOfBoxesToConnect: Int): Playground =
+    val closestPairs = Point3D.findClosestPairs(allBoxes).take(numberOfBoxesToConnect)
+    closestPairs
+      .foldLeft(this) { case (playground, (box1, box2)) =>
+        playground.connectBoxes(box1, box2)
+      }
+
+def solvePart1(boxes: Set[Point3D], numberOfBoxesToConnect: Int) =
+  val playground = Playground.initial(boxes)
+  val finalPlayground = playground.connect(numberOfBoxesToConnect)
+  finalPlayground.score
+
+def solvePart2(input: String): Int = ???
+
+object Playground:
+  def initial(boxes: Set[Point3D]): Playground =
+    val circuits = boxes.map(box => Circuit(Set(box)))
+    Playground(allBoxes = boxes, circuits = circuits)
+
+@main def day08(): Unit =
+  val input = readInput("day08/input.txt")
+
+  val boxes = Point3D.parsePoints(input).toSet
+  val part1 = solvePart1(boxes, numberOfBoxesToConnect = 1000)
+  println(s"Part 1: $part1")
+
+  val part2 = solvePart2(input)
+  println(s"Part 2: $part2")
---
+++ src/test/scala/Day08Test.scala
@@ -0,0 +1,127 @@
+import aoc2025.day08.*
+import aoc2025.utils.readInput
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+class Day08Test extends AnyFunSpec with Matchers {
+
+  val exampleInput: String =
+    """162,817,812
+      |57,618,57
+      |906,360,560
+      |592,479,940
+      |352,342,300
+      |466,668,158
+      |542,29,236
+      |431,825,988
+      |739,650,466
+      |52,470,668
+      |216,146,977
+      |819,987,18
+      |117,168,530
+      |805,96,715
+      |346,949,466
+      |970,615,88
+      |941,993,340
+      |862,61,35
+      |984,92,344
+      |425,690,689""".stripMargin
+  lazy val puzzleInput: String = readInput("day08/input.txt")
+
+  describe("Point3D") {
+    describe("parsePoints") {
+      it("should parse input into sequence of points") {
+        val input = """1,2,3
+                      |4,5,6""".stripMargin
+        Point3D.parsePoints(input) shouldBe Seq(Point3D(1, 2, 3), Point3D(4, 5, 6))
+      }
+
+      it("should parse first few points of example input") {
+        val points = Point3D.parsePoints(exampleInput)
+        points.head shouldBe Point3D(162, 817, 812)
+        points(1) shouldBe Point3D(57, 618, 57)
+        points.size shouldBe 20
+      }
+    }
+
+    describe("distanceTo") {
+      it("should return 0 for same point") {
+        val p = Point3D(1, 2, 3)
+        p.distanceTo(p) shouldBe 0.0
+      }
+
+      it("should calculate distance along single axis") {
+        Point3D(0, 0, 0).distanceTo(Point3D(3, 0, 0)) shouldBe 3.0
+        Point3D(0, 0, 0).distanceTo(Point3D(0, 4, 0)) shouldBe 4.0
+        Point3D(0, 0, 0).distanceTo(Point3D(0, 0, 5)) shouldBe 5.0
+      }
+
+      it("should calculate 3D distance") {
+        Point3D(0, 0, 0).distanceTo(Point3D(1, 2, 2)) shouldBe 3.0
+      }
+    }
+
+    describe("ordering") {
+      it("should sort by x first") {
+        val points = Seq(Point3D(2, 0, 0), Point3D(1, 0, 0), Point3D(3, 0, 0))
+        points.sorted shouldBe Seq(Point3D(1, 0, 0), Point3D(2, 0, 0), Point3D(3, 0, 0))
+      }
+
+      it("should sort by y when x is equal") {
+        val points = Seq(Point3D(1, 3, 0), Point3D(1, 1, 0), Point3D(1, 2, 0))
+        points.sorted shouldBe Seq(Point3D(1, 1, 0), Point3D(1, 2, 0), Point3D(1, 3, 0))
+      }
+
+      it("should sort by z when x and y are equal") {
+        val points = Seq(Point3D(1, 1, 3), Point3D(1, 1, 1), Point3D(1, 1, 2))
+        points.sorted shouldBe Seq(Point3D(1, 1, 1), Point3D(1, 1, 2), Point3D(1, 1, 3))
+      }
+
+      it("should sort by x, then y, then z") {
+        val points = Seq(
+          Point3D(2, 1, 1),
+          Point3D(1, 2, 1),
+          Point3D(1, 1, 2),
+          Point3D(1, 1, 1)
+        )
+        points.sorted shouldBe Seq(
+          Point3D(1, 1, 1),
+          Point3D(1, 1, 2),
+          Point3D(1, 2, 1),
+          Point3D(2, 1, 1)
+        )
+      }
+    }
+  }
+
+  describe("findClosestPairs") {
+    it("should return pairs sorted by distance on example input") {
+      val points = Point3D.parsePoints(exampleInput).toSet
+      val pairs = Point3D.findClosestPairs(points)
+
+      pairs(0) shouldBe (Point3D(162, 817, 812), Point3D(425, 690, 689))
+      pairs(1) shouldBe (Point3D(162, 817, 812), Point3D(431, 825, 988))
+      pairs(2) shouldBe (Point3D(805, 96, 715), Point3D(906, 360, 560))
+      pairs(3) shouldBe (Point3D(425, 690, 689), Point3D(431, 825, 988))
+    }
+  }
+
+  describe("solvePart1") {
+    it("should work on example input") {
+      solvePart1(Point3D.parsePoints(exampleInput).toSet, numberOfBoxesToConnect = 10) shouldBe 40
+    }
+    ignore("should work on puzzle input") {
+      solvePart1(Point3D.parsePoints(puzzleInput).toSet, numberOfBoxesToConnect = 1000) shouldBe -1
+    }
+  }
+
+  describe("solvePart2") {
+    ignore("should work on example input") {
+      solvePart2(exampleInput) shouldBe -1
+    }
+    ignore("should work on puzzle input") {
+      solvePart2(puzzleInput) shouldBe -1
+    }
+  }
+
+}
